0743bf0fc1f0b1fde2a4263c3f2481fcb104933c
ShintyaAngelicaa <shintyaangelica@gmail.com>
2024-02-05 09:16:00 +0700

    edit register

--- cypress/e2e/Quiz-3/Register/createNewAccount.js
+++ cypress/e2e/Quiz-3/Register/createNewAccount.js
@@ -1,47 +1,96 @@
+import { constants } from "buffer"
 import Register from "../../../support/pageObject/Register"
+const registerData = require("../../../fixtures/registerData.json")
 
 describe('Create New Account Functionality', () => {
   beforeEach(() => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
   })
-  it('Make sure successfully create a new account', () => {
-    cy.get(Register.fName).type('ag')
-    cy.get(Register.lName).type('af')
-    cy.get(Register.email).type('mikasa04@yopmail.com')
-    cy.get(Register.pwd).type('Shanon1216')
-    cy.get(Register.confirm_pwd).type('Shanon1216')
+  it.only('Make sure successfully create a new account', () => {
+    cy.get(Register.fName).type(registerData.newAcc.newFName)
+    cy.get(Register.lName).type(registerData.newAcc.newLName)
+    cy.get(Register.email).type(registerData.newAcc.newEmail)
+    cy.get(Register.pwd).type(registerData.newAcc.newPwd)
+    cy.get(Register.confirm_pwd).type(registerData.newAcc.newConfirmPwd)
     Register.clickCreate()
     Register.verifySuccess()
   })
+  it('Verify success create account will redirect to valid url', () => {
+    cy.get(Register.fName).type(registerData.newAcc.newFName)
+    cy.get(Register.lName).type(registerData.newAcc.newLName)
+    cy.get(Register.email).type(registerData.newAcc.newEmail)
+    cy.get(Register.pwd).type(registerData.newAcc.newPwd)
+    cy.get(Register.confirm_pwd).type(registerData.newAcc.newConfirmPwd)
+    Register.clickCreate()
+    Register.verifySuccess()
+    cy.url().should('contain', '/')
+  })
   it('Verify failed create account using registered customer', () => {
-    cy.get(Register.fName).type('shintya')
-    cy.get(Register.lName).type('simatupang')
-    cy.get(Register.email).type('mikasa00@yopmail.com')
-    cy.get(Register.pwd).type('Shanon1212')
-    cy.get(Register.confirm_pwd).type('Shanon1212')
+    cy.get(Register.fName).type(registerData.currentAcc.currentFName)
+    cy.get(Register.lName).type(registerData.currentAcc.currentLName)
+    cy.get(Register.email).type(registerData.currentAcc.currentEmail)
+    cy.get(Register.pwd).type(registerData.currentAcc.currentPwd)
+    cy.get(Register.confirm_pwd).type(registerData.currentAcc.currentConfirmPwd)
     Register.clickCreate()
     Register.verifyError()
   })
   it('Verify failed create account using registered email', () => {
-
-  })
+    cy.get(Register.fName).type(registerData.emptyCase.FName),
+    cy.get(Register.lName).type(registerData.emptyCase.LName),
+    cy.get(Register.email).type(registerData.currentAcc.currentEmail),
+    cy.get(Register.pwd).type(registerData.emptyCase.pwd),
+    cy.get(Register.confirm_pwd).type(registerData.emptyCase.confirmPwd)
+    Register.clickCreate()
+    Register.verifyError()
+    })
   it('Verify failed create account without fills all the fields', () => {
-
+    cy.get(Register.fName).type(''),
+    cy.get(Register.lName).type(''),
+    cy.get(Register.email).type(''),
+    cy.get(Register.pwd).type(''),
+    cy.get(Register.confirm_pwd).type('')
+    Register.clickCreate()
   })
   it('Verify failed create account without fills the First Name field', () => {
-
+    cy.get(Register.fName).type(''),
+    cy.get(Register.lName).type(registerData.emptyCase.LName),
+    cy.get(Register.email).type(registerData.emptyCase.email),
+    cy.get(Register.pwd).type(registerData.emptyCase.pwd),
+    cy.get(Register.confirm_pwd).type(registerData.emptyCase.confirmPwd)
+    Register.clickCreate()
   })
   it('Verify failed create account without fills the Last Name field', () => {
+    cy.get(Register.fName).type(registerData.emptyCase.FName),
+    cy.get(Register.lName).type(''),
+    cy.get(Register.email).type(registerData.emptyCase.email),
+    cy.get(Register.pwd).type(registerData.emptyCase.pwd),
+    cy.get(Register.confirm_pwd).type(registerData.emptyCase.confirmPwd)
+    Register.clickCreate()
 
   })
   it('Verify failed create account without fills the E-mail field', () => {
-
+    cy.get(Register.fName).type(registerData.emptyCase.FName),
+    cy.get(Register.lName).type(registerData.emptyCase.LName),
+    cy.get(Register.email).type(''),
+    cy.get(Register.pwd).type(registerData.emptyCase.pwd),
+    cy.get(Register.confirm_pwd).type(registerData.emptyCase.confirmPwd)
+    Register.clickCreate()
   })
   it('Verify failed create account without fills the Password field', () => {
-
+    cy.get(Register.fName).type(registerData.emptyCase.FName),
+    cy.get(Register.lName).type(registerData.emptyCase.LName),
+    cy.get(Register.email).type(registerData.emptyCase.email),
+    cy.get(Register.pwd).type(''),
+    cy.get(Register.confirm_pwd).type(registerData.emptyCase.confirmPwd)
+    Register.clickCreate()
   })
   it('Verify failed create account without fills the Confirm Password field', () => {
-
+    cy.get(Register.fName).type(registerData.emptyCase.FName),
+    cy.get(Register.lName).type(registerData.emptyCase.LName),
+    cy.get(Register.email).type(registerData.emptyCase.email),
+    cy.get(Register.pwd).type(registerData.emptyCase.pwd),
+    cy.get(Register.confirm_pwd).type('')
+    Register.clickCreate()
   })
   
 
