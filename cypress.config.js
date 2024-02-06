1d9a74c300816407ff6f9ca38f556005a713df48
isako27 <m.isakoyuda@gmail.com>
2024-02-06 17:36:17 +0700

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    
+    baseUrl: "https://magento.softwaretestingboard.com",
+    env: {
+      fname: "standard_user",
+      lname: "secret_sauce"
+    },
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     defaultCommandTimeout: 5432,
     viewportWidth: 900,
-    viewportHeight: 650,
+    viewportHeight: 650
+    
 
 
   },
---
+++ cypress/support/pageObject/EditAccount Page.js
@@ -0,0 +1,19 @@
+class EditAccountPage{
+    //cara 1
+    //yang disimpan hanya element locator
+    fname = '#firstname'
+    lname = '#lastname'
+    login_btn = '.submit-button.btn_action'
+    err = '[data-test="error"]'
+
+    //cara 2
+    //buat fungsi lengkap
+    verifyError(){
+        cy.get(this.err).should('be.visible')
+    }
+
+    clickSave(){
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+    }
+}
+export default new EditAccountPage()
