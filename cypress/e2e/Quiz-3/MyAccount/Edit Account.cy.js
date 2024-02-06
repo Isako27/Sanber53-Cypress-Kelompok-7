6c393b19559cded651d00c016f63881bb9fe04e9
isako27 <m.isakoyuda@gmail.com>
2024-02-06 21:14:52 +0700

    My Account

--- cypress/e2e/Quiz-3/MyAccount/Edit Account.cy.js
+++ cypress/e2e/Quiz-3/MyAccount/Edit Account.cy.js
@@ -1,18 +1,34 @@
-describe('Edit Account spec', () => {
-  it('Succes Edit Account Information', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
+describe('Test Menu Edit Account', () => {
 
-    cy.get('#firstname').type ('Tsubasa')
-    cy.get('#lastname').type ('Ozora')
-    cy.get('#change-email')
-    cy.get('#change-password')
-    cy.get('#current-password').type ('12345')
-    cy.get('#password').type ('Jl. Raya Soreang No. 36')
-    cy.get('#city').type ('Bandung')
-    cy.get('#region').type ('Jawa Barat')
-    cy.get('#password-confirmation').type ('44004')
-    cy.get('#country').type ('Indonesia')
+ // branch "main" 
+
+  beforeEach(() => {
+   
+   cy.visit('https://magento.softwaretestingboard.com')
+
+  })
+   
+   it('Edit Account Information - Edit FirstName & LastName', () => {
+    
+    // Login
+    cy.contains('Sign In').click()
+    cy.get('#email').type('ihsansolusi@gmail.com')
+    cy.get('#pass').type('Mochamad27')
+    cy.wait(1000)
+    cy.get('#send2').click()
+
+    // ke menu ganti account
+    //branch "main"
+    cy.visit('https://magento.softwaretestingboard.com/customer/account')
+    
+    cy.get('.base').should('contain','My Account')
+    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+    cy.get('#firstname').clear()
+    cy.get('#firstname').type("Mochamad Isako")
+    cy.get('#lastname').clear()
+    cy.get('#lastname').type("Yuda")
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+    cy.get('.message-success > div').should('have.text','You saved the account information.')
 
 
   })
